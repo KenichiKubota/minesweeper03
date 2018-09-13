7bad21e93e34ce94a174a8c805055b1151c1d1c3
KenichiKubota <kenichi.l.kubota@gmail.com>
2018-09-13 17:02:07 +0900

    first commit

---
+++ MineSweeper03/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace MineSweeper03
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+
+            // セルのインスタンスを一つずつ作る
+            myCell1 = new MyCell(1, 0, 0, true);
+            myCell2 = new MyCell(2, 30, 0, false);
+            myCell3 = new MyCell(3, 60, 0, false);
+            myCell4 = new MyCell(4, 0, 30, false);
+            myCell5 = new MyCell(5, 30, 30, false);
+            myCell6 = new MyCell(6, 60, 30, false);
+            myCell7 = new MyCell(7, 0, 60, false);
+            myCell8 = new MyCell(8, 30, 60, false);
+            myCell9 = new MyCell(9, 60, 60, false);
+
+            // セルにその周囲のセルをセットする
+            myCell1.AddAroundCells(myCell2, myCell4, myCell5);
+            myCell2.AddAroundCells(myCell1, myCell3, myCell4, myCell5, myCell6);
+            myCell3.AddAroundCells(myCell2, myCell5, myCell6);
+            myCell4.AddAroundCells(myCell1, myCell2, myCell5, myCell7, myCell8);
+            myCell5.AddAroundCells(myCell1, myCell2, myCell3, myCell4, myCell6, myCell7, myCell8, myCell9);
+            myCell6.AddAroundCells(myCell2, myCell3, myCell5, myCell8, myCell9);
+            myCell7.AddAroundCells(myCell4, myCell5, myCell8);
+            myCell8.AddAroundCells(myCell4, myCell5, myCell6, myCell7, myCell9);
+            myCell9.AddAroundCells(myCell5, myCell6, myCell8);
+
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+
+            // セルを画面に表示する
+            this.Controls.Add(this.myCell1);
+            this.Controls.Add(this.myCell2);
+            this.Controls.Add(this.myCell3);
+            this.Controls.Add(this.myCell4);
+            this.Controls.Add(this.myCell5);
+            this.Controls.Add(this.myCell6);
+            this.Controls.Add(this.myCell7);
+            this.Controls.Add(this.myCell8);
+            this.Controls.Add(this.myCell9);
+
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MyCell myCell1;
+        private MyCell myCell2;
+        private MyCell myCell3;
+        private MyCell myCell4;
+        private MyCell myCell5;
+        private MyCell myCell6;
+        private MyCell myCell7;
+        private MyCell myCell8;
+        private MyCell myCell9;
+
+    }
+}
+
---
+++ MineSweeper03/MyCell.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MineSweeper03
+{
+    public partial class MyCell : CheckBox
+    {
+
+        // 通し番号
+        private int SeqNum;
+
+        // 爆弾かどうかを判定するフラグ（爆弾ならtrue）
+        private Boolean BombFlag;
+
+        // このセルの周囲のセルを格納するリスト
+        private List<MyCell> aroundCells = new List<MyCell>();
+
+        //------------------------------------------
+        // 周囲のセルを一気にセットする
+        //------------------------------------------
+        public void AddAroundCells(params MyCell[] aroundCells)
+        {
+            foreach(MyCell arroundCell in aroundCells)
+            {
+                this.aroundCells.Add(arroundCell);
+            }
+        }
+
+        public MyCell(int SeqNum, int locationX,int locationY,Boolean BombFlag) :base()
+        {
+            //InitializeComponent();
+            //base.Appearance = Appearance.Button;
+            //base.Margin = new System.Windows.Forms.Padding(0);
+            //this.AutoSize = true;
+            //this.Location = new System.Drawing.Point(locationX, locationY);
+            //this.Size = new System.Drawing.Size(30, 30);
+            //base.TabIndex = 1;
+            //this.UseVisualStyleBackColor = true;
+            base.Appearance = Appearance.Button;
+            base.Margin = new System.Windows.Forms.Padding(0);
+            base.Size = new System.Drawing.Size(30, 30);
+            base.TabIndex = 1;
+            base.UseVisualStyleBackColor = true;
+            base.Location = new System.Drawing.Point(locationX, locationY);
+            this.BombFlag = BombFlag;
+            this.SeqNum = SeqNum;
+        }
+
+        public MyCell(IContainer container)
+        {
+            InitializeComponent();
+        }
+
+        //------------------------------------------
+        // クリックされたときに起こるイベント
+        //------------------------------------------
+        protected override void OnMouseDown(MouseEventArgs mevent)
+        {
+            switch (mevent.Button)
+            {
+                // 左クリックしたとき
+                case MouseButtons.Left:
+                    this.OnLeftClick();
+                    break;
+                // 右クリックしたとき
+                case MouseButtons.Right:
+                    this.OnRightClick();
+                    break;
+
+            }
+        }
+
+        //------------------------------------------
+        // 左クリックしたときの処理
+        //------------------------------------------
+        private void OnLeftClick()
+        {
+            // チェック済みのときは即終了
+            if (base.Checked)
+            {
+                return;
+            }
+
+            // チェックする
+            base.Checked = true;
+
+            // 爆弾なら「B」を表示
+            if (BombFlag == true)
+            {
+                base.Text = "B";
+            }
+            else
+            {
+
+                // 周囲の爆弾の数をカウントする
+                int ArroundBombCount = 0;
+
+                foreach (MyCell aroundCell in aroundCells)
+                {
+                    if(aroundCell.BombFlag)
+                    {
+                        ArroundBombCount++;
+                    }
+                }
+
+                
+                if (ArroundBombCount == 0)
+                {
+                    // 周囲の爆弾の数が0なら、周囲のセルを左クリックする
+                    foreach (MyCell aroundCell in aroundCells)
+                    {
+                        aroundCell.OnLeftClick();
+                    }
+                }
+                else
+                {
+                    // 周囲の爆弾の数が0でなければ、その数を表示
+                    base.Text = ArroundBombCount.ToString();
+                }
+                
+            }
+        }
+
+        //------------------------------------------
+        // 右クリックしたときの処理
+        //------------------------------------------
+        private void OnRightClick()
+        {
+
+        }
+    }
+}
